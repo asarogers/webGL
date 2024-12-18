69880a94b369cc381b1a9c895032d4fb9b5d027f
Asa Rogers <cyberasasoftware@gmail.com>
2024-12-18 13:43:25 -0600

    add intro file

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import HelloWorld from './sections/HelloWorld';
+
+function App() {
+  return (
+    <div className="App">
+      <HelloWorld />
+    </div>
+  );
+}
+
+export default App;
